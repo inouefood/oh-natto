a25a1d6f1437a0ec338f16510a038b473ca9cdc5
ch64ng <aki.maps@gmail.com>
2019-04-07 16:32:50 +0900

    refactor: add some initializers

--- natto/Extension/SKSpriteNode+Addition.swift
+++ natto/Extension/SKSpriteNode+Addition.swift
@@ -9,7 +9,7 @@
 import SpriteKit
 
 extension SKSpriteNode {
-    // initializer for MixScene
+    // initializer for MixScene/mame, ohashi
     convenience init(image: String, pos: CGPoint, body: SKPhysicsBody, category: UInt32, contact: UInt32, isGravity: Bool) {
         self.init(imageNamed: image)
         self.position = pos
@@ -18,11 +18,27 @@ extension SKSpriteNode {
         self.physicsBody?.contactTestBitMask = contact
         self.physicsBody!.affectedByGravity = isGravity
     }
-    // initializer for PullNattoScene
+    // initializer for PullNattoScene/mame
     convenience init(image: String, pos: CGPoint, body: SKPhysicsBody, isGravity: Bool) {
         self.init(imageNamed: image)
         self.position = pos
         self.physicsBody = body
         self.physicsBody!.affectedByGravity = isGravity
+    }
+    // initializer for PullNattoScene/mouth
+    convenience init(image: String, viewBounds: CGRect, frame: CGRect, zPos: CGFloat) {
+        self.init(imageNamed: image)
+        let scale = viewBounds.width / self.size.width
+        self.position = CGPoint(x: frame.midX, y: frame.maxY - (self.size.height * scale * 2.0) / 2.0)
+        self.size = CGSize(width: frame.size.width, height: self.size.height * scale * 2.0)
+        self.zPosition = zPos
+    }
+    // initializer for PullNattoScene/ohashi
+    convenience init(image: String, pos: CGPoint, viewBounds: CGRect, frame: CGRect, zPos: CGFloat) {
+        self.init(imageNamed: image)
+        self.position = pos
+        let scale = viewBounds.width / self.size.width
+        self.size = CGSize(width: self.size.width * scale / 2.0, height: self.size.height * scale / 2.0)
+        self.zPosition = zPos
     }
 }
