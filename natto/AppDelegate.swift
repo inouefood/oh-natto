5d34ca85d0ae7898b72b3d21394453933cced2ca
佐川晴海 <sagawaharumi@sagawaharuminoMacBook-puro.local>
2019-07-02 21:37:05 +0900

    feat: Add local notification

--- natto/AppDelegate.swift
+++ natto/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -20,7 +21,45 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         self.window?.rootViewController = NattoViewController()
         self.window?.makeKeyAndVisible()
         
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .badge, .sound]) {(granted, error) in
+            if granted {
+                print("許可する")
+            } else {
+                print("許可しない")
+            }
+        }
+        setLocalNotification(title:"今日は納豆の日です", message:"oh! natto!で納豆を混ぜてみませんか？",month: 7, day: 10)
         return true
+    }
+    
+    private func setLocalNotification(title:String = "", message:String, month: Int, day: Int,hour:Int = 9, minute:Int = 0, second:Int = 0 ){
+        // タイトル、本文、サウンド設定の保持
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.body = message
+        content.sound = UNNotificationSound.default
+        
+        var notificationTime = DateComponents()
+        notificationTime.month = month
+        notificationTime.day = day
+        notificationTime.hour = hour
+        notificationTime.minute = minute
+        notificationTime.second = second
+        
+        let trigger: UNNotificationTrigger = UNCalendarNotificationTrigger(dateMatching: notificationTime, repeats: false)
+        
+        // 識別子とともに通知の表示内容とトリガーをrequestに内包
+        let request = UNNotificationRequest(identifier: "Natto", content: content, trigger: trigger)
+        
+        // UNUserNotificationCenterにrequestを加える
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        center.add(request) { (error) in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+        }
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
@@ -46,5 +85,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate{
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        // アプリ起動中でもアラートと音で通知
+        completionHandler([.alert, .sound])
+    }
 }
 
