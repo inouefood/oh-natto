b7413c35895d185c49140f337343b04822a0b5c5
佐川晴海 <sagawaharumi@sagawaharuminoMacBook-puro.local>
2019-10-05 23:35:43 +0900

    feat: Add localize file

--- natto/AppDelegate.swift
+++ natto/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        
         self.window = UIWindow(frame: UIScreen.main.bounds)
         self.window?.rootViewController = NattoViewController()
         self.window?.makeKeyAndVisible()
