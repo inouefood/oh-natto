082975fb434da5ddfaaaf8ea78fcd650fb115366
佐川晴海[YUMEMI] / Sagawa Harumi <h_sagawa@yumemi.co.jp>
2020-09-29 18:53:43 +0900

    説明画面のUIの中身のデータを繋ぎ込みました

--- natto/Scene/Description/DescriptionView.swift
+++ natto/Scene/Description/DescriptionView.swift
@@ -10,12 +10,30 @@ import UIKit
 
 class DescriptionView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override init(frame: CGRect) {
+       super.init(frame: frame)
+       loadNib()
     }
-    */
+   
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        loadNib()
+    }
+    
+    func loadNib() {
+        //CustomViewの部分は各自作成したXibの名前に書き換えてください
+        let view = Bundle.main.loadNibNamed("DescriptionView", owner: self, options: nil)?.first as! UIView
+        view.frame = self.bounds
+        self.addSubview(view)
+    }
+
+    
+    func setDescription(image: UIImage?, text:String) {
+        imageView.image = image
+        descriptionLabel.text = text
+    }
 
 }
--- natto/Scene/Description/DescriptionViewController.swift
+++ natto/Scene/Description/DescriptionViewController.swift
@@ -11,10 +11,30 @@ import UIKit
 class DescriptionViewController: UIViewController {
 
     
-    // - MARK: LifeCycle
+    @IBOutlet weak var firstPage: DescriptionView! {
+        didSet {
+            
+            firstPage.setDescription(image: UIImage(named: "description1"),
+                                     text: localizeString(key: LocalizeKeys.Description.one))
+        }
+    }
+    @IBOutlet weak var secondPage: DescriptionView!{
+        didSet {
+            secondPage.setDescription(image: UIImage(named: "description2"),
+                                      text: localizeString(key: LocalizeKeys.Description.two))
+        }
+    }
+    @IBOutlet weak var therdPage: DescriptionView!{
+        didSet {
+            therdPage.setDescription(image: UIImage(named: "description3"),
+                                     text: localizeString(key: LocalizeKeys.Description.three))
+        }
+    }
     
+    // - MARK: LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .clear
     }
  
 }
