4755ed7d7610cf5206a439efa42aa13ab9d959fb
佐川晴海[YUMEMI] / Sagawa Harumi <h_sagawa@yumemi.co.jp>
2020-09-29 18:12:12 +0900

    説明詳細表示ようのcustomViewを作成しました

---
+++ natto/Scene/Description/DescriptionView.swift
@@ -0,0 +1,21 @@
+//
+//  DescriptionView.swift
+//  natto
+//
+//  Created by 佐川 晴海 on 2020/09/29.
+//  Copyright © 2020 佐川　晴海. All rights reserved.
+//
+
+import UIKit
+
+class DescriptionView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
