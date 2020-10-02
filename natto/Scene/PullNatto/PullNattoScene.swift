7aa3c2bf6b48873c73c5ad296e5c93ba08c7927c
佐川晴海[YUMEMI] / Sagawa Harumi <h_sagawa@yumemi.co.jp>
2020-10-02 23:15:28 +0900

    refactor: pullNattoSceneで読みづらくなっている箇所のフォーマットを修正しました

--- natto/Scene/PullNatto/PullNattoScene.swift
+++ natto/Scene/PullNatto/PullNattoScene.swift
@@ -27,11 +27,19 @@ class PullNattoScene: SKScene{
         }
         return sprites
         }()
-    lazy var ohashi = SKSpriteNode(image: "pullOhashi", pos: CGPoint(x: self.frame.midX, y: self.frame.midY), viewBounds: (self.view?.bounds)!, frame: self.frame, zPos: 1.25)
-    lazy var mouth = SKSpriteNode(image: "pakupaku", viewBounds: (self.view?.bounds)!, frame: self.frame, zPos: 1.0)
+    
+    lazy var ohashi = SKSpriteNode(image: "pullOhashi",
+                                   pos: CGPoint(x: self.frame.midX, y: self.frame.midY),
+                                   viewBounds: (self.view?.bounds)!,
+                                   frame: self.frame,
+                                   zPos: 1.25)
+    
+    lazy var mouth = SKSpriteNode(image: "pakupaku",
+                                  viewBounds: (self.view?.bounds)!,
+                                  frame: self.frame,
+                                  zPos: 1.0)
     
     // MARK: - Property
-    
     var timer:Timer?
     var score:Int = 0
     let stickyLevel:Float
@@ -40,21 +48,21 @@ class PullNattoScene: SKScene{
         let presenter = PullNattoPresenterImpl(output: self, model: PullNattoModel())
         presenter.loadBgmAudio(resourceName:"natto_bgm_game", resourceType: "wav")
         presenter.loadEffectAudio(resourceName: "paku", resourceType: "wav")
+        
         return presenter
     }()
     
     // MARK: - Initializer
-    
     init(size: CGSize, sticky: Int) {
         stickyLevel = Float(sticky) * Constant.Sticky.level
         super.init(size: size)
     }
+    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
     //MARK: - LifeCycle
-    
     override func didMove(to view: SKView) {
         //原点の変更
         self.anchorPoint = CGPoint(x: 0, y: 0)
@@ -64,34 +72,17 @@ class PullNattoScene: SKScene{
         
         presenter.playBgm()
 
-        self.timer = Timer.scheduledTimer(timeInterval: 25, target: self, selector: #selector(MixScene.timerCounter), userInfo: nil, repeats: true)
+        self.timer = Timer.scheduledTimer(timeInterval: 25,
+                                          target: self,
+                                          selector: #selector(MixScene.timerCounter),
+                                          userInfo: nil,
+                                          repeats: true)
 
         self.addChild(mouth, ohashi)
+        
         nattoSprite.forEach({
             self.addChild($0)
         })
-    }
-    
-    //MARK: - Event
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        ohashi.position = CGPoint(x: pos.x, y: pos.y)
-    }
-
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    @objc func timerCounter(){
-        self.timer?.invalidate()
-        
-        for i in 0..<nattoSprite.count {
-            if height < nattoSprite[i].position.y {
-                score += 1
-            }
-        }
-        presenter = nil
-        let scene = ResultScene(size: self.size, score: score)
-        self.view!.presentScene(scene)
     }
     
     override func update(_ currentTime: TimeInterval) {
@@ -102,20 +93,50 @@ class PullNattoScene: SKScene{
         }
         
         for (i, natto) in nattoSprite.enumerated() {
-            presenter.eatCheck(height: Float(height), nattoY: Float(natto.position.y), index: i)
+            presenter.eatCheck(height: Float(height),
+                               nattoY: Float(natto.position.y),
+                               index: i)
             
-            presenter.updateNattoPosition(ohashiPos: ObjectPosition(pos: ohashi.position), ohashiSize: ObjectSize(size: ohashi.size), nattoPos: ObjectPosition(pos: natto.position), sticky: stickyLevel, dist: Float(minLength/10), index: i)
+            presenter.updateNattoPosition(ohashiPos: ObjectPosition(pos: ohashi.position),
+                                          ohashiSize: ObjectSize(size: ohashi.size),
+                                          nattoPos: ObjectPosition(pos: natto.position),
+                                          sticky: stickyLevel,
+                                          dist: Float(minLength/10),
+                                          index: i)
         }
+    }
+    
+    //MARK: - Event
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches {
+            let pos = t.location(in: self)
+            ohashi.position = CGPoint(x: pos.x, y: pos.y)
+            
+        }
+    }
+    
+    @objc func timerCounter(){
+        self.timer?.invalidate()
+        
+        for i in 0..<nattoSprite.count {
+            if height < nattoSprite[i].position.y {
+                score += 1
+            }
+        }
+        presenter = nil
+        
+        let scene = ResultScene(size: self.size, score: score)
+        self.view!.presentScene(scene)
     }
 }
 
 // MARK: - PullNattoPresenterOutput
-
 extension PullNattoScene: PullNattoPresenterOutput {
     func showUpdateNatto(objPos: ObjectPosition, index: Int) {
         nattoSprite[index].position.x += CGFloat(objPos.x)
         nattoSprite[index].position.y += CGFloat(objPos.y)
     }
+    
     func showEatNatto(index: Int){
         nattoSprite[index].position.y = height + 100
         presenter.playEffect()
