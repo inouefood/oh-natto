96f0312dbb7087430bac14134f88559b96d45ef0
佐川晴海[YUMEMI] / Sagawa Harumi <h_sagawa@yumemi.co.jp>
2020-10-07 18:45:26 +0900

    feat:大まかなレイアウトを作成しました

--- natto/Scene/Result/ResultScene.swift
+++ natto/Scene/Result/ResultScene.swift
@@ -6,8 +6,6 @@
 //  Copyright © 2018年 佐川　晴海. All rights reserved.
 //
 import SpriteKit
-import StoreKit
-import AVFoundation
 
 class ResultScene: SKScene{
     
@@ -16,29 +14,11 @@ class ResultScene: SKScene{
         return SKLabelNode(fontSize: 100, text: localizeString(key: LocalizeKeys.Result.buttonRelpay),
                            pos: CGPoint(x: self.frame.midX, y: height * 0.20))
     }()
-    
-    lazy var bestScoreLabel: SKLabelNode! = {
-        if let bestScore = UserStore.bestScore {
-            return SKLabelNode(fontSize: 50,
-                               text: localizeString(key: LocalizeKeys.Result.bestScore) + String(bestScore),
-                               pos: CGPoint(x: self.frame.midX, y: height * 0.85))
-        }
-        
-        let node = SKLabelNode()
-        node.isHidden = true
-        return node
-    }()
-    
-    lazy var scoreLabel: SKLabelNode! = {
-        return SKLabelNode(fontSize: 100,
-                           text: localizeString(key: LocalizeKeys.Result.score) + String(resultScore),
-                           pos: CGPoint(x: self.frame.midX,
-                                        y: height * 0.90))
-    }()
+
 
     // MARK: - Property
     let resultScore:Int
-    var audio: AVAudioPlayer!
+    
     
     // MARK: - Initializer
     init(size:CGSize, score: Int) {
@@ -54,47 +34,26 @@ class ResultScene: SKScene{
     override func didMove(to view: SKView) {
         super.didMove(to: view)
         
+        let vc = ResultViewController(score: resultScore)
+        vc.dismissHandler = {
+            SNSShareData.shared.button.isHidden = true
+            
+            
+            vc.dismiss(animated: false, completion: nil)
+            let scene = TitleScene(size: self.size)
+            view.presentScene(scene)
+        }
+        vc.modalPresentationStyle = .overCurrentContext
+        topViewController()?.present(vc, animated: false, completion: nil)
+        return
+        
         setScreenInit()
         
-        //値の保存
-        UserStore.saveEatPoint(natto: resultScore)
-        UserStore.totalNattoCount += resultScore
-        
-        //レビューダイアログの表示
-        if UserStore.totalNattoCount > 1000 && UserStore.isNeedDisplayedReviewAlert {
-            UserStore.isNeedDisplayedReviewAlert = false
-            SKStoreReviewController.requestReview()
-        }
-        
-        //紙吹雪表示処理の判定
-        guard let bestScore = UserStore.bestScore else {
-            UserStore.bestScore = resultScore
-            return
-        }
-        if resultScore > bestScore {
-            let vc = BestScoreViewController(score: resultScore)
-            vc.modalPresentationStyle = .overCurrentContext
-            topViewController()?.present(vc, animated: false, completion: nil)
-            return
-        }
-        
     }
     
-    // MARK: - PrivateMethod
-    private func loadAudio(resourceName: String, resourceType: String) {
-        let path = Bundle.main.path(forResource: resourceName, ofType: resourceType)
-        let url = URL(fileURLWithPath: path!)
-        do { try  audio = AVAudioPlayer(contentsOf: url) }
-        catch{ fatalError() }
-        audio.numberOfLoops = -1
-        audio.prepareToPlay()
-        audio.play()
-    }
+   
     
     private func setScreenInit() {
-        loadAudio(resourceName: "natto_bgm_score.wav", resourceType: "")
-        
-        self.addChild(scoreLabel, bestScoreLabel, replayLabel)
         
         let mamekun = SKSpriteNode(image: "mame01", pos: CGPoint(x:width/2,y: height/2))
         let animation = SKAction.animate(with:[SKTexture(imageNamed: "mame01"),
--- natto/View/Result/ResultViewController.swift
+++ natto/View/Result/ResultViewController.swift
@@ -7,12 +7,88 @@
 //
 
 import UIKit
+import StoreKit
+import AVFoundation
 
 class ResultViewController: UIViewController {
+    private var audio: AVAudioPlayer!
+    private var score: Int
+    
+    var dismissHandler: (()->())?
+    
+    @IBOutlet weak var scoreLabel: UILabel! {
+        didSet {
+            scoreLabel.text = localizeString(key: LocalizeKeys.Result.score) + String(score)
+        }
+    }
+    @IBOutlet weak var bestScoreLabel: UILabel! {
+        didSet {
+            guard let bsetScore = UserStore.bestScore else {
+                bestScoreLabel.isHidden = true
+                return
+            }
+            bestScoreLabel.text = localizeString(key: LocalizeKeys.Result.bestScore) + bsetScore.description
+        }
+    }
+    @IBOutlet weak var totalNattoCountLabel: UILabel!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    // MARK: - Initializer
+    init(score: Int) {
+        self.score = score
+        //値の保存
+        UserStore.saveEatPoint(natto: score)
+        UserStore.totalNattoCount += score
+        
+        super.init(nibName: String(describing: ResultViewController.self), bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.backgroundColor = UIColor(red: 38/255, green: 38/255, blue: 38/255, alpha: 1.0)
+        loadAudio(resourceName: "natto_bgm_score.wav", resourceType: "")
+        
+        //レビューダイアログの表示
+        if UserStore.totalNattoCount > 1000 && UserStore.isNeedDisplayedReviewAlert {
+            UserStore.isNeedDisplayedReviewAlert = false
+            SKStoreReviewController.requestReview()
+        }
+        
+        let totalNattoCount = UserStore.totalNattoCount
+        totalNattoCountLabel.text = totalNattoCount.description + "粒"
+        
+        //紙吹雪表示処理の判定
+        guard let bestScore = UserStore.bestScore else {
+            UserStore.bestScore = score
+            return
+        }
+        
+        if score > bestScore {
+            let vc = BestScoreViewController(score: score)
+            vc.modalPresentationStyle = .overCurrentContext
+            present(vc, animated: false, completion: nil)
+        }
+    }
+    
+    // MARK: - PrivateMethod
+    private func loadAudio(resourceName: String, resourceType: String) {
+        let path = Bundle.main.path(forResource: resourceName, ofType: resourceType)
+        let url = URL(fileURLWithPath: path!)
+        do { try  audio = AVAudioPlayer(contentsOf: url) }
+        catch{ fatalError() }
+        audio.numberOfLoops = -1
+        audio.prepareToPlay()
+        audio.play()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        dismissHandler?()
     }
 
 }
