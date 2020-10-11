a63097c866f577156a581f17804948b0e1709ccf
佐川晴海[YUMEMI] / Sagawa Harumi <h_sagawa@yumemi.co.jp>
2020-10-11 18:19:13 +0900

    feat: トッピング購入用のVCを作成

---
+++ natto/View/Store/StoreViewController.swift
@@ -0,0 +1,19 @@
+//
+//  StoreViewController.swift
+//  natto
+//
+//  Created by 佐川 晴海 on 2020/10/11.
+//  Copyright © 2020 佐川　晴海. All rights reserved.
+//
+
+import UIKit
+
+class StoreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
